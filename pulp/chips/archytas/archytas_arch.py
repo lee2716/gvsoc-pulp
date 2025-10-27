9daec33bb89cdf20d00a8aa62e48c8677193683f
Alessandro <alessandro.nadalini3@unibo.it>
2025-10-27 14:42:39 +0100

    [WIP]: first draft of A-Tile

---
+++ pulp/chips/archytas/archytas_arch.py
@@ -0,0 +1,49 @@
+#
+# Copyright (C) 2025 ETH Zurich, University of Bologna and Fondazione ChipsIT
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+class ArchytasArch:
+    # Single tile address map from magia_tile_pkg.sv
+    RESERVED_ADDR_START = 0x0000_0000
+    RESERVED_SIZE       = 0x0000_FFFF - 0x0000_00E0
+    RESERVED_ADDR_END   = RESERVED_ADDR_START + RESERVED_SIZE
+    PCM_START           = RESERVED_ADDR_END + 1
+    PCM_SIZE            = 0x0000_00E0
+    PCM_END             = 0x0000_FFFF
+    STACK_ADDR_START    = PCM_END + 1
+    STACK_SIZE          = 0x0000_FFFF
+    STACK_ADDR_END      = STACK_ADDR_START + STACK_SIZE
+    L1_ADDR_START       = STACK_ADDR_END + 1
+    L1_SIZE             = 0x000D_FFFF
+    L1_ADDR_END         = L1_ADDR_START + L1_SIZE
+    L1_TILE_OFFSET      = 0x0010_0000
+    L2_ADDR_START       = 0xC000_0000
+    L2_SIZE             = 0x4000_0000
+    L2_ADDR_END         = L2_ADDR_START + L2_SIZE
+    STDOUT_START        = 0xFFFF_0004
+    STDOUT_SIZE         = 0x100
+
+    # From magia_pkg.sv
+    N_MEM_BANKS         = 32        # Number of TCDM banks
+    N_WORDS_BANK        = 8192      # Number of words per TCDM bank
+
+    # Extra
+    BYTES_PER_WORD      = 4
+    TILE_CLK_FREQ       = 50 * (10 ** 6)
+
+    ENABLE_NOC          = True
+    N_TILES_X           = 2 # 16
+    N_TILES_Y           = 2 # 16
+    NB_CLUSTERS         = N_TILES_X*N_TILES_Y
---
+++ pulp/chips/archytas/archytas_core.py
@@ -0,0 +1,46 @@
+#
+# Copyright (C) 2025 ETH Zurich, University of Bologna and Fondazione ChipsIT
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+import gvsoc.systree
+import cpu.iss.riscv
+from pulp.chips.archytas.archytas_isa import *
+from cpu.iss.isa_gen.isa_smallfloats import *
+
+# RV32 core with X-if
+class CV32CoreTest(cpu.iss.riscv.RiscvCommon):
+    def __init__(self, parent: gvsoc.systree.Component, name: str, binaries: list=[],
+                 fetch_enable: bool=False, boot_addr: int=0, timed: bool=True,
+                 core_id: int=0):
+
+        # Properties
+        isa_str = 'rv32imafc'
+        misa = 0x40000000
+        debug_handler = 0x1a190800
+        fetch_enable = False
+        riscv_exceptions = True
+
+        # Instantiates the ISA from the string.
+        isa = cpu.iss.isa_gen.isa_riscv_gen.RiscvIsa('cv32-base', isa_str, extensions=[iDMA_Ctrl(), Rv32redmule(), FSync(), Xf16alt(), Xf16()])
+
+        super().__init__(parent, name, isa=isa, misa=misa, core_id=core_id,
+                         debug_handler=debug_handler, fetch_enable=fetch_enable,
+                         riscv_exceptions=riscv_exceptions)
+
+        # TODO check later
+        self.add_c_flags([
+            "-DPIPELINE_STALL_THRESHOLD=1",
+            "-DCONFIG_ISS_CORE=riscv",
+        ])
